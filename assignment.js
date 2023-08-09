245bb108764ded697fc9b34242ca14ed2aa9c991
Gazi2050 <gazi45344@gmail.com>
2023-08-09 12:14:39 +0600

    problem-2-solved

--- assignment.js
+++ assignment.js
@@ -12,3 +12,23 @@ function cubeNumber(number) {
 const inputNumber = 5;
 const result = cubeNumber(inputNumber);
 console.log(`The cube of the number ${inputNumber} is:`, result);
+
+// problem-2
+function matchFinder(string1, string2) {
+    if (typeof string1 != 'string') {
+        return "Please provide a  string in string1";
+    }
+    else if (typeof string2 != 'string') {
+        return "Please provide a  string in string2";
+    }
+    for (let i = 0; i <= string1.length - string2.length; i++) {
+        if (string1.slice(i, i + string2.length) === string2) {
+            return true;
+        }
+    }
+    return false;
+}
+const firstString = "John Doe";
+const secondString = "ohn";
+const result1 = matchFinder(firstString, secondString);
+console.log(result1);
